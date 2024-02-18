e1be56e5862be11d0abcf72cf06fd4e7a4c656fc
Aubin Detrez <aubin.detrez@gmail.com>
2024-02-18 17:42:06 +0100

    Add label to the tree when hit

--- HitBox.gd
+++ HitBox.gd
@@ -2,13 +2,25 @@ extends StaticBody2D
 class_name HitBox # Detect hit by player, have life and drop resources
 
 @export var audioPlayer: AudioStreamPlayer
+@export var label: Label
+
+@onready var timer: Timer = $Timer
 
 func _ready():
-	pass
+	label.visible = false
 
 func _process(delta):
 	pass
 
 func _receive_attack(power):
 	print("Tree is hit by power:", power)
+	display_label_1s()
 	audioPlayer.play()
+
+func display_label_1s():
+	label.visible = true
+	timer.wait_time = 1.0
+	timer.start()
+
+func _on_timer_timeout():
+	label.visible = false
