132ce773a63ea82140bdb88bba8ac19672b5c384
Aubin Detrez <aubin.detrez@gmail.com>
2024-02-24 20:06:52 +0100

    Drop rocks when the Rock is hit

--- HitBox.gd
+++ HitBox.gd
@@ -13,6 +13,9 @@ func _receive_attack(_power):
 	display_label_1s()
 	audioPlayer.play()
 
+	await audioPlayer.finished
+	instanciate_item()
+
 func display_label_1s():
 	label.visible = true
 	timer.wait_time = 1.0
@@ -20,3 +23,12 @@ func display_label_1s():
 
 func _on_timer_timeout():
 	label.visible = false
+
+@export var drop_offset: Vector2 = Vector2(0, 70)
+func instanciate_item():
+	var drop_scene = preload("res://dropped_item.tscn")
+	var instance = drop_scene.instantiate()
+	instance.global_position = self.global_position - drop_offset
+	instance.friction = 3
+	instance.item_name = "rock"
+	get_tree().get_current_scene().add_child(instance)
